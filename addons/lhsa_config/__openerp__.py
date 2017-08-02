51650e028e55d5aaee25edce1c06c9ff655d17c9
Michael Karrer <michaelkarrer81@gmail.com>
2017-08-03 00:32:50 +0200

    [ADD] __openerp__.py for the default instance addon

---
+++ addons/lhsa_config/__openerp__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'lhsa_config',
+    'summary': '''FS-Online lhsa instance configuration''',
+    'description': '''
+FS-Online Instance Configuration
+================================
+
+Customer configuration for the instance lhsa
+
+- Default settings
+- View modifications
+- CSS
+- Translations
+    ''',
+    'author': 'Michael Karrer (michael.karrer@datadialog.net), DataDialog',
+    'version': '1.0',
+    'website': 'https://www.datadialog.net',
+    'installable': True,
+    'depends': [
+        'fsonline',
+    ],
+    'data': [],
+}
+
+    
